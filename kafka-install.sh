786d72015d86643af6c2a8e2fce580afc93cf858
KazinAS <KazinAS@gnivc.ru>
2022-06-28 08:55:38 +0300

    kafka install script

--- kafka-install.sh
+++ kafka-install.sh
@@ -1,2 +1,9 @@
-$ KAFKA_VERSION=2.12-2.2.1 && \
-wget -q -O - https://www-us.apache.org/dist/kafka/2.2.1/kafka_${KAFKA_VERSION}.tgz | (cd /opt; tar -zxvf -) && cd /opt/kafka_${KAFKA_VERSION}
+#!/bin/bash
+#
+# Kafka install
+
+$ KAFKA_VERSION=2.12-2.2.1 &&
+  wget -q -O - https://www-us.apache.org/dist/kafka/2.2.1/kafka_${KAFKA_VERSION}.tgz | (
+    cd /opt
+    tar -zxvf -
+  ) && cd /opt/kafka_${KAFKA_VERSION}
